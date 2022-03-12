4cb0becede13836b80bab199e23609f6a01b1209
charan200415 <66833883+charan200415@users.noreply.github.com>
2022-03-12 11:52:52 +0530

    Update server.py

--- deploy-container/server.py
+++ deploy-container/server.py
@@ -1,31 +1,35 @@
 import os
 print('running')
-def system(x):
+def ser(x):
     os.system(x)
 def download():
     import requests
     url = 'https://mustyklutzyconfiguration.charan200415.repl.co/download'
-    r = requests.get(url, allow_redirects=True)
-    open('test.tar.gz', 'wb').write(r.content)
-    system('tar -xv -f test.tar.gz')
-    system('rm test.tar.gz')
-download()
+    ser(f'curl -o test.tar.gz {url}')
+    ser('tar -xvf test.tar.gz')
+    ser('rm test.tar.gz')
+#download()
 def upload():    
-    system('tar chvfz test.tar.gz *')
+    ser('tar chvfz test.tar.gz project')
     import requests
     url = 'https://mustyklutzyconfiguration.charan200415.repl.co/'
     file='test.tar.gz'
     files = {'file1': (file, open(file, 'rb'))}
     response = requests.post(url, files=files)
-    system('rm test.tar.gz')
+    ser('rm test.tar.gz')
 from time import sleep
-from IPython.display import clear_output
 from datetime import datetime
 import pytz
 while True:
     india = pytz.timezone('Asia/Kolkata')
     india_time = datetime.now(india).strftime('%Y:%m:%d %H:%M:%S')
-    clear_output(wait=True)
+    x=True
+    if x:
+        with open('project/log','w') as file:
+            file.write('')
+        x=False
+    with open('project/log','a') as file:
+        file.write('Uploaded Below Files At '+str(india_time))
     print('Uploaded Below Files At '+str(india_time))
     upload()
     print('sleeping')
